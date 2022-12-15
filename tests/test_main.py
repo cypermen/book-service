3f902796afd09295bdd79423b87986b93f72dfae
cypermen <41022014+cypermen@users.noreply.github.com>
2022-12-16 00:26:12 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,14 +8,12 @@ def test_user_empty_get():
     s = requests.Session()
     id = '7bd047cb-a57e-412c-9d83-81c50e3e3902'
     response = s.get(f'{api}/find/{id}')
-    print(response.status_code)
     assert response.status_code == 200
 
 def test_user_save_and_get():
     s = requests.Session()
     parameters = "?title=cypermen&authorId=7bd047cb-a57e-412c-9d83-81c50e3e3902"
     response = s.post(f'{api}/add/{parameters}')
-    print(response.status_code)
     assert response.status_code == 200
     assert response.json().get('title') == 'cypermen'
     createdBookId = response.json().get('id')
